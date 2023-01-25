8867b50947b16ff699143a28ac230dc30895e3af
Mitchell Gantz <mgantz22@icloud.com>
2023-01-25 10:20:54 -0800

    Add business logic

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-parseInt(Math.random()*6)
+
+function Dice() {
+  this.number = 0;
+  this.totalScore = 0;
+  this.roundScore = 0;
+}
+
+let dice1 = new Dice();
+let dice2 = new Dice();
+
+Dice.prototype.Roll = function() {
+  this.number = Math.ceil(Math.random()*6);
+}
+
+Dice.prototype.PlayRound() {
+    if (this.number !== 1) {
+        this.roundScore += this.number;
+    } else {
+        this.roundScore = 0;
+    }
+    return this.roundScore;
+}
+
+Dice.prototype.TotalRound() {
+    this.roundScore += this.totalScore;
+
+}
+
+Dice.prototype.DetermineWinner() {
+    if(this.TotalScore >= 100 ){
+        console.log(this.totalScore + ", you won!");
+    }
+
+}
---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+function Dice() {
+    this.number = 0;
+    this.totalScore = 0;
+    this.roundScore = 0;
+}
+
+let dice1 = new Dice();
+let dice2 = new Dice();
+
+Dice.prototype.Roll = function() {
+    this.number = Math.ceil(Math.random()*6);
+}
+
+Dice.prototype.PlayRound = function() {
+    if (this.number !== 1) {
+        this.roundScore += this.number;
+    } else {
+        this.roundScore = 0;
+    }
+    return this.roundScore;
+}
+
+Dice.prototype.TotalRound = function() {
+    this.roundScore += this.totalScore;
+}
+
+Dice.prototype.DetermineWinner = function() {
+    if(this.TotalScore >= 100 ){
+        console.log(this.totalScore + ", you won!");
+    }
+
+}
